4287d4f07a4dc1b359782ce79a2abbe166e14f93
Oleksandr Rad <clavigo365@gmail.com>
2024-11-14 12:14:18 +0200

    add solution 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,7 +4,5 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-spider.style.position = 'relative';
-wall.style.display = 'flex';
-wall.style.justifyContent = 'center';
-wall.style.alignItems = 'center';
+spider.style.top = `${wall.clientHeight / 2 - spider.clientHeight / 2}px`;
+spider.style.left = `${wall.clientWidth / 2 - spider.clientWidth / 2}px`;
